bd28ec058246906005614be3aa1938b51c2f71a8
sovamainer <104361108+sovamainer@users.noreply.github.com>
2023-10-24 23:49:29 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
 
-using namespace std;
-int main()
-{
-    setlocale(0, "");
-    cout << "int: " << sizeof(int) << "\n";
-    cout << "short int: " << sizeof(short int) << "\n";
-    cout << "long int: " << sizeof(long int) << "\n";
-    cout << "float: " << sizeof(float) << "\n";
-    return(0);
-}
